7c835be6c5de383bd08227f44dec918ddb3bc2e5
CJ <chirag7jain@gmail.com>
2015-09-17 21:23:49 +0530

    Printing move no for each move

--- tower_of_hanoi.rb
+++ tower_of_hanoi.rb
@@ -1,11 +1,12 @@
 # Tower Hanoi Problem
 # No of moves for n is f(n) = 2^n - 1
-def toh(n, source, destination, intermediate)
+def toh(n, source, destination, intermediate, count = 1)
   if n == 1
-    puts "#{source} to #{destination}"
+    puts "(#{count} move) #{source} to #{destination}"
+    count = count + 1
   elsif n > 1
-    toh(n-1, source, destination, intermediate)
-    toh(1, source, intermediate, destination)
-    toh(n-1, intermediate, destination, source)
+    count = toh(n-1, source, destination, intermediate, count)
+    count = toh(1, source, intermediate, destination, count)
+    count = toh(n-1, intermediate, destination, source, count)
   end
 end
