40a1f4c65b2d837652b9e3c705453b6385ce8b46
CJ <chirag7jain@gmail.com>
2016-10-08 12:31:46 +0530

    identicalPairs

---
+++ identicalPair.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Created by cj on 7/10/16.
+ */
+class identicalPairs{
+
+    static <T extends Comparable<T>> int getCount(ArrayList<T> items){
+        if (items.size() <= 1)
+            return 0;
+        else if (items.size() > 2)
+            Collections.sort(items);
+
+        int count = 0;
+        T lastKey = items.get(0);
+        int lastKeyCount = 0;
+        for(int i=0;i<items.size();i++){
+            if (lastKey != items.get(i)){
+                count += (lastKeyCount * (lastKeyCount-1))/2;
+                lastKey = items.get(i);
+                lastKeyCount = 0;
+            }
+            lastKeyCount++;
+        }
+        return count;
+    }
+}
+
+class identicalPair {
+    static ArrayList<Integer> items = new ArrayList<>(Arrays.asList(1,2,3,1,1,3,5,1,2));
+
+    public static void main(String args[]){
+        System.out.print(identicalPairs.getCount(items));
+    }
+
+
+}
